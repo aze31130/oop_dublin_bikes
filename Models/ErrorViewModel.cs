6a06b9cf442beb9ad83a85b0ab076a1bb9663cbf
Guilhem <guilhem.carlet@epita.fr>
2021-03-15 16:36:28 +0000

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace oop_dublin_bikes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
