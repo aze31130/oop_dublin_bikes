1022f6655d899c02358cb18f12bfa7389473a0c1
Marin BAILHE <marin.bailhe@epita.fr>
2021-03-21 15:53:32 +0000

    added bike class

---
+++ Models/Bike.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace oop_dublin_bikes.Models
+{
+    public class Bike
+    {
+        [Key]
+        public int _id { get; set; }
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
