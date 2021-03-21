f83b02dd2ee7344401dd68d7c52fed4ad85a0372
Guilhem <guilhem.carlet@epita.fr>
2021-03-21 15:59:30 +0000

    Added PUT and DELETE requests in BikesController

--- Controllers/BikesController.cs
+++ Controllers/BikesController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using oop_dublin_bikes.Data;
+using oop_dublin_bikes.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace oop_dublin_bikes.Controllers
 {
@@ -11,6 +15,37 @@ namespace oop_dublin_bikes.Controllers
         public BikesController(Context context)
         {
             _context = context;
+        }
+
+        [HttpPut("id")]
+        public async Task<ActionResult> UpdateBike(int id, Bike bike)
+        {
+            if (!id.Equals(bike._id) || !_context.Bikes.Any(x => x._id.Equals(id))
+            {
+                return BadRequest();
+            }
+            else
+            {
+                _context.Entry(bike).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetBikes", new { id = bike._id }, bike);
+            }
+        }
+
+        [HttpDelete("id")]
+        public async Task<ActionResult<Bike>> DeleteBike(int id)
+        {
+            var bike = await _context.Bikes.FindAsync(id);
+            if (bike == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.Bikes.Remove(bike);
+                await _context.SaveChangesAsync();
+                return bike;
+            }
         }
     }
 }
