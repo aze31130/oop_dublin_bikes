7b6eeb8f09bd58fe0bfe3d3a6278589ba54f3235
Marin BAILHE <marin.bailhe@epita.fr>
2021-03-21 15:57:12 +0000

    added table name

--- Data/Context.cs
+++ Data/Context.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using oop_dublin_bikes.Models;
 
 namespace oop_dublin_bikes.Data
 {
     public class Context : DbContext
     {
         public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<Bike> Bikes { get; set; }
     }
 }
