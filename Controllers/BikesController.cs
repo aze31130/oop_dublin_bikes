5fc12bb83ce4a7c565d28781a3d1dc8192c44b45
Guilhem <guilhem.carlet@epita.fr>
2021-03-21 15:29:53 +0000

    Added MySql Context

---
+++ Controllers/BikesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using oop_dublin_bikes.Data;
+
+namespace oop_dublin_bikes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BikesController : ControllerBase
+    {
+        private readonly Context _context;
+        public BikesController(Context context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ Data/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace oop_dublin_bikes.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+    }
+}
